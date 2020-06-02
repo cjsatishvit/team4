b7abd6066c6d554a70e4f4a951ced01c246faf01
akancha12 <47851459+akancha12@users.noreply.github.com>
2020-06-03 00:18:53 +0530

    Report generation

--- BankLoanProject/src/GenerateReport.java
+++ BankLoanProject/src/GenerateReport.java
@@ -1,6 +1,12 @@
 
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,17 +20,33 @@ public class GenerateReport extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    String url="";
+    String username="";
+    String pwd="";
 
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+//		doGet(request, response);
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection(url,username,pwd);
+			String sql="SELECT amount_collected, date, customer FROM table";
+			PreparedStatement st=con.prepareStatement(sql);
+			ResultSet rs=st.executeQuery();
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
